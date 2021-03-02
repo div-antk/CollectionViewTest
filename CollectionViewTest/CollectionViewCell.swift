1a1d28ec208b7d10c033b2adc0fe05ccbe4c8709
div-antk <51283468+owaii@users.noreply.github.com>
2021-03-02 21:35:58 +0900

    セルの繋ぎこみ

---
+++ CollectionViewTest/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewTest
+//
+//  Created by Takuya Ando on 2021/03/02.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- CollectionViewTest/ViewController.swift
+++ CollectionViewTest/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let nib = UINib(nibName: "CollectionViewCell", bundle: Bundle.main)
+        
+        collectionView.register(nib, forCellWithReuseIdentifier: "cardCell")
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
-
+    // セクションの数
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardCell", for: indexPath) as! CollectionViewCell
+        
+        return cell
+    }
 }
 
