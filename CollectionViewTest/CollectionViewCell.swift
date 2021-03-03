8d00a9736458a6f52297d6fb2e06b83632298d29
div-antk <51283468+owaii@users.noreply.github.com>
2021-03-03 22:18:00 +0900

    プログレスバー途中

--- CollectionViewTest/CollectionViewCell.swift
+++ CollectionViewTest/CollectionViewCell.swift
@@ -13,6 +13,8 @@ class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var progressBar: UIProgressView!
     
+    var count = 0.0
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -25,6 +27,25 @@ class CollectionViewCell: UICollectionViewCell {
         mainCard.layer.shadowRadius = 8
         mainCard.layer.shadowOffset = CGSize(width: 4, height: 4)
         
+        
+        // プログレスバー
+        let progressBar = UIProgressView(progressViewStyle: .default)
+        
+        // 進捗の割合（0.0～1.0）
+//        progressBar.progress = 1.0
+        
+        // 進捗の割合の設定（0.0～1.0 ）とアニメーションの有無
+        progressBar.setProgress(Float(count), animated: true)
+        
+        // スタイル
+        progressBar.progressViewStyle = .bar
+        
+        // バーの色を取得
     }
 
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        
+        
+    }
 }
